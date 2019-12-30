989297dc2f26d878555931633e97421a122cfcc8
Sagar Jadhav <sagar.d.jadhav@accenture.com>
2019-12-30 16:11:10 +0530

    Adding new topics

---
+++ Day1/js/strActivity.js
@@ -0,0 +1,9 @@
+var str = "JS Script script SCRIPT sCRIPT";
+
+var pos1 = str.search(/script/i);
+
+console.log(pos1);
+
+var str1 = "It can be tough to know where to start learning";
+
+console.log(str1.substr(32,15));
