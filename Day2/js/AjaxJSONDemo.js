c298a76602f7900d9749bab07c5d9a8323498d3f
Sagar Jadhav <sagar.d.jadhav@accenture.com>
2019-12-31 17:57:14 +0530

    New Content on Ajax added

--- Day2/js/AjaxJSONDemo.js
+++ Day2/js/AjaxJSONDemo.js
@@ -5,10 +5,11 @@ function sendRequest(){
     xmlhttp.onreadystatechange = function(){
         if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
             var content = document.getElementById("content");
-            content.innerHTML = xmlhttp.responseText;
+            var jsonObj = JSON.parse(this.responseJSON);
+            content.innerHTML = jsonObj;
         }
     }
 
-    xmlhttp.open("GET", "emp.txt", true);
+    xmlhttp.open("GET", "emp.json", true);
     xmlhttp.send();
 }
